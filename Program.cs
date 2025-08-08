e86b1e4e045a46b0cff257f6b1c555dad06fa1fa
jeroen <jeroen.vanschijndel@otoy.com>
2025-08-08 16:27:49 +0200

    scalars and vectors

---
+++ Program.cs
@@ -0,0 +1,313 @@
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace mathgen
+{
+    public class Scalar
+    {
+        public enum Type
+        {
+            Int,
+            UInt,
+            Float,
+        }
+
+        public Type type;
+        public int bitness;
+
+        public string stdtype
+        {
+            get
+            {
+                switch (type)
+                {
+                    case Type.Int:
+                        return $"int{bitness}_t";
+                    case Type.UInt:
+                        return $"uint{bitness}_t";
+                    case Type.Float:
+                        if (bitness == 32)
+                            return $"float";
+                        if (bitness == 64)
+                            return $"double";
+                        break;
+                }
+                return "UNKNOWN";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                switch (type)
+                {
+                    case Type.Int:
+                        return $"i{bitness}";
+                    case Type.UInt:
+                        return $"u{bitness}";
+                    case Type.Float:
+                        return $"f{bitness}";
+                }
+                return "UNKNOWN";
+            }
+        }
+
+        public string Declaration
+        {
+            get
+            {
+                return $"using {Name} = {stdtype};\n";
+            }
+        }
+    };
+
+    public class Vector
+    {
+        public Scalar type;
+        public int width;
+        public string Name
+        {
+            get
+            {
+                return $"{type.Name}_{width}";
+            }
+        }
+
+        public string Ctors
+        {
+            get
+            {
+                string str = "";
+                str += $"\t{Name}(";
+                for (int i = 0; i < width; i++)
+                {
+                    char[] letters = { 'x', 'y', 'z', 'w' };
+                    str += $"{type.Name} _{letters[i]}";
+                    str += (i != width - 1) ? ", " : "";
+                }
+                str += ") : ";
+                for (int i = 0; i < width; i++)
+                {
+                    char[] letters = { 'x', 'y', 'z', 'w' };
+                    str += $"{letters[i]}(_{letters[i]})";
+                    str += (i != width - 1) ? ", " : "";
+                }
+                str += "{}\n";
+                return str;
+            }
+        }
+        public string Operator1(string op)
+        {
+            string str = "";
+            str += $"\tfriend {Name} operator {op} (const {Name}& a)";
+            str += $" {{ return {Name}(";
+            for (int i = 0; i < width; i++)
+            {
+                char[] letters = { 'x', 'y', 'z', 'w' };
+                str += $"{op}a.{letters[i]}";
+                str += (i != width - 1) ? ", " : "";
+            }
+            str += "); }";
+            str += "\n";
+            return str;
+        }
+        public string Operator2s(string op)
+        {
+            string str = "";
+            str += $"\tfriend {Name} operator {op} (const {Name}& a, const {type.Name}& b)";
+            str += $" {{ return {Name}(";
+            for (int i = 0; i < width; i++)
+            {
+                char[] letters = { 'x', 'y', 'z', 'w' };
+                str += $"a.{letters[i]} {op} b";
+                str += (i != width - 1) ? ", " : "";
+            }
+            str += "); }";
+            str += "\n";
+            return str;
+        }
+
+        public string Operator2(string op)
+        {
+            string str = "";
+            str += $"\tfriend {Name} operator {op} (const {Name}& a, const {Name}& b)";
+            str += $" {{ return {Name}(";
+            for (int i = 0; i < width; i++)
+            {
+                char[] letters = { 'x', 'y', 'z', 'w' };
+                str += $"a.{letters[i]} {op} b.{letters[i]}";
+                str += (i != width - 1) ? ", " : "";
+            }
+            str += "); }";
+            str += "\n";
+            return str;
+        }
+        public string PerComp(string name, string func)
+        {
+            string str = "";
+            str += $"\tfriend {Name} {name} (const {Name}& a, const {Name}& b)";
+            str += $" {{ return {Name}(";
+            for (int i = 0; i < width; i++)
+            {
+                char[] letters = { 'x', 'y', 'z', 'w' };
+                str += $"{func}(a.{letters[i]}, b.{letters[i]})";
+                str += (i != width - 1) ? ", " : "";
+            }
+            str += "); }";
+            str += "\n";
+            return str;
+        }
+        public string PerComp(string name) { return PerComp(name, name); }
+        public string Functions()
+        {
+            string str = "";
+            if (type.type == Scalar.Type.Float)
+                str += FloatFunctions();
+
+            str += PerComp("min", "std::min");
+            str += PerComp("max", "std::max");
+
+            return str;
+        }
+        public string Operators()
+        {
+            string str = "";
+            str += Operator1("-");
+            str += Operator2s("+");
+            str += Operator2("+");
+            str += Operator2s("-");
+            str += Operator2("-");
+            str += Operator2s("*");
+            str += Operator2("*");
+            str += Operator2s("/");
+            str += Operator2("/");
+            return str;
+        }
+        public string MinFunction()
+        {
+            string str = "";
+            return str;
+        }
+        public string FloatFunctions()
+        {
+            string str = "";
+            str += DotFunction();
+            str += LengthFunction();
+
+            if (width == 3)
+                str += CrossFunction();
+
+            return str;
+        }
+
+        public string DotFunction()
+        {
+            string str = "";
+            str += $"\tfriend {type.Name} dot(const {Name}& a, const {Name}& b) {{ ";
+            str += $"return ";
+            for (int i = 0; i < width; i++)
+            {
+                char[] letters = { 'x', 'y', 'z', 'w' };
+                str += $"a.{letters[i]} * b.{letters[i]}";
+                str += (i != width - 1) ? " + " : "";
+            }
+            str += "; }\n";
+            return str;
+        }
+
+        public string LengthFunction()
+        {
+            string str = "";
+            str += $"\tfriend {type.Name} length(const {Name}& a) {{ ";
+            str += $"return sqrt(dot(a, a));";
+            str += " }}\n";
+            return str;
+        }
+        public string CrossFunction()
+        {
+            string str = "";
+            str += $"\tfriend {Name} cross(const {Name}& a, const {Name}& b) {{ ";
+            str += $"return {Name}(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)";
+            str += "}}\n";
+            return str;
+        }
+
+
+        public string Declaration
+        {
+            get
+            {
+                string str = $"export struct {Name}\n{{\n";
+                str += $"\t{type.Name} ";
+                for (int i = 0; i < width; i++)
+                {
+                    char[] letters = { 'x', 'y', 'z', 'w' };
+                    str += $"{letters[i]}";
+                    str += (i != width - 1) ? ", " : ";\n";
+                }
+
+                str += Ctors;
+                str += Operators();
+                str += Functions();
+
+                str += "};\n";
+
+
+                return str;
+            }
+        }
+    };
+
+    public class Matrix
+    {
+        public Vector type;
+        public int height;
+        public string Name
+        {
+            get
+            {
+                return $"{type.Name}_{height}";
+            }
+        }
+    };
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Scalar> scalars = new List<Scalar>();
+            for (int i = 0; i < 4; i++)
+            {
+                int[] bitness = { 8, 16, 32, 64 };
+                scalars.Add(new Scalar { type = Scalar.Type.Int, bitness = bitness[i] });
+                scalars.Add(new Scalar { type = Scalar.Type.UInt, bitness = bitness[i] });
+                if (i > 1)
+                    scalars.Add(new Scalar { type = Scalar.Type.Float, bitness = bitness[i] });
+            }
+            List<Vector> vectors = new List<Vector>();
+            foreach (var s in scalars)
+            {
+                vectors.Add(new Vector { type = s, width = 2 });
+                vectors.Add(new Vector { type = s, width = 3 });
+                vectors.Add(new Vector { type = s, width = 4 });
+            }
+
+            string code = "";
+            code += "module;\n";
+            code += "#include <stdint.h>\n";
+            code += "#include <algorithm>\n";
+            code += "export module types;\n";
+
+            foreach (var s in scalars)
+                code += s.Declaration;
+
+            foreach (var v in vectors)
+                code += v.Declaration;
+
+            System.IO.File.WriteAllText("types.ixx", code);
+        }
+    };
+}
